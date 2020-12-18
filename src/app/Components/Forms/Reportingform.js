7d0479cb5faa0579478db749ca0746b9e4433d5e
Dawar Ejaz <dawar33011@gmail.com>
2020-12-19 02:15:44 +0500

    Update Reportingform.js

--- src/app/Components/Forms/Reportingform.js
+++ src/app/Components/Forms/Reportingform.js
@@ -62,100 +62,73 @@ export class Reportingform extends React.Component {
   render() {
     return (
       <div className="App">
-        <Container>
-          <Row>
-            <Col xs="2" lg="2" xl="2">
-              <Image
-                src={toAbsoluteUrl("/media/logos/TMC.jpg")}
-                className="App-logo"
-                alt="logo"
-              />
-            </Col>
-            <Col col-lg-6 col-xxl-4>
-              REPORT
-            </Col>
-            {/* <Col xs lg="2">
-            <Button variant="primary">Primary</Button>
-            </Col> */}
-          </Row>
-          <Row>
-            <p class="font-weight-bold">Table</p>
-            <Table striped bordered hover variant="dark">
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Warden Name</th>
-                  <th>Area of Duty</th>
-                  <th>Time</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Jacob</td>
-                  <td>Thornton</td>
-                  <td>@fat</td>
-                </tr>
-                <tr>
-                  <td>3</td>
-                  <td colSpan="2">Larry the Bird</td>
-                  <td>@twitter</td>
-                </tr>
-              </tbody>
-            </Table>
-          </Row>
-          <div className="chart">
-            <Row>
-              <Col lg xs={6}>
-                <LineChart />
-              </Col>
-              <Col lg xs={6}>
-                <PieChart />
-              </Col>
-              <br></br>
-              <br></br>
-              <br></br>
-              <Col lg xs={12}>
-                <p>Mostly Congested Areas</p>
-                <Table striped bordered hover variant="dark">
-                  <thead>
-                    <tr>
-                      <th>#</th>
-                      <th>Area</th>
-                      <th>Time</th>
-                      <th>Congestion</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>1</td>
-                      <td>Mark</td>
-                      <td>Otto</td>
-                      <td>@mdo</td>
-                    </tr>
-                    <tr>
-                      <td>2</td>
-                      <td>Jacob</td>
-                      <td>Thornton</td>
-                      <td>@fat</td>
-                    </tr>
-                    <tr>
-                      <td>3</td>
-                      <td colSpan="2">Larry the Bird</td>
-                      <td>@twitter</td>
-                    </tr>
-                  </tbody>
-                </Table>
-              </Col>
-            </Row>
-          </div>
-        </Container>
+        <p class="font-weight-bold">Table</p>
+        <Table striped bordered hover variant="dark">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Warden Name</th>
+              <th>Area of Duty</th>
+              <th>Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td colSpan="2">Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </Table>
+        <div className="chart">
+          <LineChart />
+          <PieChart />
+          <br></br>
+          <br></br>
+          <br></br>
+          <p>Mostly Congested Areas</p>
+          <Table striped bordered hover variant="dark">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Area</th>
+                <th>Time</th>
+                <th>Congestion</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td colSpan="2">Larry the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </Table>
+        </div>
       </div>
     );
   }
