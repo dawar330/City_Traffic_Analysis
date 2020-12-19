c08c3283cdaebce63ed645f2e2570201d646faac
Dawar Ejaz <dawar33011@gmail.com>
2020-12-19 15:23:08 +0500

    Update ReportingPage.js

--- src/app/pages/ReportingPage.js
+++ src/app/pages/ReportingPage.js
@@ -2,10 +2,18 @@ import React, { useRef } from "react";
 import { useSubheader } from "../../_metronic/layout";
 import Reportingform from "../Components/Forms/Reportingform";
 import { useReactToPrint } from "react-to-print";
+import { shallowEqual, useSelector } from "react-redux";
 
 export const ReportingPage = () => {
   const suhbeader = useSubheader();
   const componentRef = useRef();
+  const { Duties } = useSelector(
+    ({ Responsibility }) => ({
+      Duties: Responsibility.Duties,
+    }),
+    shallowEqual
+  );
+  console.log("page", Duties);
 
   const handlePrint = useReactToPrint({
     content: () => componentRef.current,
@@ -16,7 +24,7 @@ export const ReportingPage = () => {
   return (
     <div className="row">
       <div className="col-xxl-12 order-2 order-xxl-1">
-        <Reportingform ref={componentRef} />
+        <Reportingform Duties={Duties} ref={componentRef} />
         <button class="btn btn-primary" size="lg" onClick={handlePrint}>
           Print
         </button>
